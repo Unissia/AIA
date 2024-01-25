66931b6c6a96b81520c1c183c74e8bab3fbf099a
Bastien BAUDOUIN <bastien.baudouin@etu.univ-nantes.fr>
2024-01-25 19:31:02 +0100

    Détection de la limite inférieure du gras

--- src/utils.py
+++ src/utils.py
@@ -32,27 +32,40 @@ def measureFatThickness(img):
 
     return l'épaisseur de la couche de gras
     """
+    # Seuils
+    fatRedThresold = 120
+    meatRedDifferenceThresold = 50
+
     # Recadrage manuel autour du nucleus medius
     img = reframeImage(img, 0, 300, 0, 400)
 
     # Recherche des pixels qui délimitent la couche de gras
     for x in range(0, img.shape[1]):
         background = True
-        previousPixelRed = img[0][x][2]
-        for y in range(0, img.shape[0]):
-            # Détection de la fin de l'arrière plan sombre
+        fatRed = -1
+        for y in range(10, img.shape[0]):
+            # Traitement sur l'arrière plan sombre
             if (background):
-                if (img[y][x][2] > 100):
+                if (img[y][x][2] > fatRedThresold):
                     background = False
+                    img[y][x][1] = 255
             # Traitements sur la couche de gras
             else:
-                if (abs(int(img[y][x][2]) - int(previousPixelRed)) > 50):
-                    img[y][x][1] = 255
+                if (fatRed == -1):
+                    # On récupère le niveau de rouge qui sera utilisé en référence
+                    fatRed = img[y+5][x][2]
 
-                previousPixelRed = img[y][x][2]
+                if (abs(int(img[y][x][2]) - int(fatRed)) > meatRedDifferenceThresold):
+                    img[y][x][1] = 255
+                    break
+
+                    # ToDo: Ajuster la méthode de comparaison des niveaux de rouge pour éviter les ratés sur des zones moins différentes
+                    # Il faudrait peut-être comparer la différence de rouge avec plusieurs pixels précédents
 
     # Calcul de l'épaisseur minimale de la couche de gras
 
+    cv.namedWindow("Display window", cv.WINDOW_NORMAL)
+    cv.resizeWindow("Display window", 800, 1000)
     cv.imshow("Display window", img)
     k = cv.waitKey(0) # Wait for a keystroke in the window
     return 0
